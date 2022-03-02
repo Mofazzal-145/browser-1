9673b6dba6911efbe7bffa6146d0e2e2ca3b676f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-03 00:05:09 +0600

    synchronous & asynchronosu

---
+++ js/fetch.js
@@ -0,0 +1,13 @@
+
+console.log('mofu');
+console.log('01755236630');
+console.log('017503504');
+setTimeout(() => console.log('mofazzal'), 5000)
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+console.log('0173124641361');
+console.log('01756461341');
+for(let i = 0; i< 10; i++){
+    console.log(i);
+}
