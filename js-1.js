b221ee6b797b37755fa385d9b6ff7b5c801b360a
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-02 23:41:59 +0600

    first js file

---
+++ js-1.js
@@ -0,0 +1,22 @@
+ function doSomething() {
+     console.log('I am coding JavaScript');
+ }
+console.log('my name is mofazzal hossain');
+console.log('my roommate name SA');
+// doSomething()
+// setTimeout(doSomething)
+// setTimeout(doSomething, 4000)
+
+
+// with function
+
+setTimeout(function(){
+    console.log('I am here');
+}, 5000)
+
+// with arrow function
+setTimeout(() =>{
+    console.log('I am using VS code');
+}, 4000)
+console.log('There is a table');
+console.log('I am explore JS');
